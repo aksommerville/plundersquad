910ae4cb894812e4db6c5b5d9cd2e7c6a72dc76d
Andy Sommerville <aksommerville@gmail.com>
2017-11-18 23:49:30 -0500

    Desert graphics

--- src/main/ps_main.c
+++ src/main/ps_main.c
@@ -43,9 +43,9 @@ static int ps_setup_test_game(int playerc,int difficulty,int length,int test_scg
 
   if (test_scgen) {
     if (ps_game_generate_test(ps_game,
-      1, // regionid
+      2, // regionid
       // blueprintids. You must provide at least one with a HERO POI.
-      2
+      2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19
     )<0) return -1;
   } else {
     if (ps_game_generate(ps_game)<0) return -1;
@@ -90,7 +90,7 @@ static int ps_main_init() {
   if (!(ps_gui=ps_gui_new())) return -1;
   if (ps_gui_set_game(ps_gui,ps_game)<0) return -1;
 
-  if (1) { // Nonzero for normal interactive setup, zero for quick testing setup
+  if (0) { // Nonzero for normal interactive setup, zero for quick testing setup
     if (ps_gui_load_page_assemble(ps_gui)<0) return -1;
   } else {
     if (ps_setup_test_game(

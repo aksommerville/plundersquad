f548aa62ecf4ced4f3f1043c215ff0dd0eb2cce2
Andy Sommerville <aksommerville@gmail.com>
2018-05-03 23:07:05 -0400

    More blueprints

--- src/main/ps_main.c
+++ src/main/ps_main.c
@@ -52,7 +52,7 @@ static struct ps_perfmon *ps_perfmon=0;
 static int ps_setup_test_game(struct ps_userconfig *userconfig) {
   int i;
 
-  if (0) { // Normal interactive setup.
+  if (1) { // Normal interactive setup.
     return 0;
   }
 
@@ -65,8 +65,8 @@ static int ps_setup_test_game(struct ps_userconfig *userconfig) {
 
   /* Optionally override plrdef selection. (plrid,plrdefid,palette,device) */
   //001-swordsman 002-archer    003-gadgeteer 004-nurse     005-wizard    006-vampire   007-martyr    008-immortal  009-bomber
-  if (ps_game_configure_player(ps_game,1,2,1,0)<0) return -1;
-  //if (ps_game_configure_player(ps_game,2,2,0,0)<0) return -1;
+  if (ps_game_configure_player(ps_game,1,2,2,0)<0) return -1;
+  //if (ps_game_configure_player(ps_game,2,8,0,0)<0) return -1;
   //if (ps_game_configure_player(ps_game,3,8,1,0)<0) return -1;
   //if (ps_game_configure_player(ps_game,4,2,2,0)<0) return -1;
 
@@ -81,7 +81,7 @@ static int ps_setup_test_game(struct ps_userconfig *userconfig) {
     if (ps_game_generate_test(ps_game,
       -1, // regionid, negative means random
       // blueprintid. At least one must have adequate HERO POI:
-      2,68,69
+      2,70,71
     )<0) return -1;
   }
 

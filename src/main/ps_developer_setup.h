2354d2ec7e997334829ad1147e037024f86f22a8
Andy Sommerville <aksommerville@gmail.com>
2018-05-30 22:14:33 -0400

    More blueprints based on solution analysis

--- src/main/ps_developer_setup.h
+++ src/main/ps_developer_setup.h
@@ -8,7 +8,7 @@
 /* Declare the heroes you want. This also establishes the party size.
  * Available: SWORDSMAN, ARCHER, GADGETEER, NURSE, WIZARD, VAMPIRE, MARTYR, IMMORTAL, BOMBER, BALA
  */
-#define PS_TEST_GAME_PARTY SWORDSMAN,IMMORTAL,BOMBER,VAMPIRE,WIZARD
+#define PS_TEST_GAME_PARTY WIZARD,GADGETEER
 
 /* Declare the scenario generator mode.
  * One of: NORMAL, SELECTED, ALL
@@ -24,7 +24,7 @@
  * Must name at least one blueprint with a HERO POI.
  * Region may be <0 to select randomly.
  */
-#define PS_TEST_BLUEPRINTS  2,144
+#define PS_TEST_BLUEPRINTS  2,146
 #define PS_TEST_REGION     -1
 
 /* Parameters for ALL mode.

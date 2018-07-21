d6c4dc0a098f7b4d59d64161ef1b66ccadd199e1
Andy Sommerville <aksommerville@gmail.com>
2018-07-20 22:36:03 -0400

    Review thru blueprint:198

--- src/main/ps_developer_setup.h
+++ src/main/ps_developer_setup.h
@@ -8,7 +8,7 @@
 /* Declare the heroes you want. This also establishes the party size.
  * Available: SWORDSMAN, ARCHER, GADGETEER, NURSE, WIZARD, VAMPIRE, IMMORTAL, BOMBER
  */
-#define PS_TEST_GAME_PARTY SWORDSMAN,ARCHER,VAMPIRE,BOMBER
+#define PS_TEST_GAME_PARTY SWORDSMAN,ARCHER,BOMBER
 
 /* Declare the scenario generator mode.
  * One of: NORMAL, SELECTED, ALL

aksommerville/plundersquad

commit e609468df7715181d6226d55aea15be6a6f1dacd

Use maptm only if all buttons exist (untested)

src/input/ps_input_maptm.c
@@ -52,6 +52,44 @@ int ps_input_maptm_set_namepattern(struct ps_input_maptm *maptm,const char *src,
   return 0;
 }
 
+/* Confirm that all of this map template's source buttons actually exist on the input device.
+ */
+ 
+struct ps_input_maptm_all_buttons_exist_on_device_ctx {
+  const struct ps_input_maptm *maptm;
+  uint8_t bits[16];
+};
+ 
+static int ps_input_maptm_all_buttons_exist_on_device_cb(
+  struct ps_input_device *device,const struct ps_input_btncfg *btncfg,void *userdata
+) {
+  struct ps_input_maptm_all_buttons_exist_on_device_ctx *ctx=userdata;
+  int fldp=ps_input_maptm_fld_search(ctx->maptm,btncfg->srcbtnid);
+  if (fldp<0) return 0;
+  if (fldp>=128) return 0;
+  ctx->bits[fldp>>3]|=(1<<(fldp&7));
+  return 0;
+}
+ 
+static int ps_input_maptm_all_buttons_exist_on_device(const struct ps_input_maptm *maptm,const struct ps_input_device *device) {
+  if (!device->report_buttons) {
+    return 1; // Assume it's OK.
+  }
+  struct ps_input_maptm_all_buttons_exist_on_device_ctx ctx={
+    .maptm=maptm,
+  };
+  int err=device->report_buttons((struct ps_input_device*)device,&ctx,ps_input_maptm_all_buttons_exist_on_device_cb);
+  if (err<0) return 0;
+  
+  int i=maptm->fldc;
+  if (i>=128) i=127;
+  while (i-->0) {
+    if (!(ctx.bits[i>>3]&(1<<(i&7)))) return 0;
+  }
+  
+  return 1;
+}
+
 /* Match device.
  */
  
@@ -59,6 +97,10 @@ int ps_input_maptm_match_device(const struct ps_input_maptm *maptm,const struct 
   if (!maptm||!device) return 0;
 
   if (maptm->invalid_provider) return 0;
+  
+  if (!ps_input_maptm_all_buttons_exist_on_device(maptm,device)) {
+    return 0;
+  }
   
   int score;
   if (maptm->namepatternc) {

TODO
@@ -6,7 +6,6 @@ General errors:
 - assemblepage: wizard's magic appeared near top-left corner...?
   ...investigated, can't reproduce
 - Monsters with new path selection (eg dragonbug, giraffe, ...), prevent walking offscreen.
-- Before assigning map to device, confirm that the input buttons actually exist.
   
 Raspi errors:
 

src/input/ps_input_event.c
@@ -168,11 +168,11 @@ int ps_input_inhibit_screensaver() {
   #if PS_USE_glx
     return ps_glx_inhibit_screensaver();
   #elif PS_USE_bcm
-    //TODO bcm screensaver -- is there such a thing? I know the console does blank after a time.
+    // Unnecessary. Even with X11 running, BCM video apparently takes precedence.
   #elif PS_USE_macwm
-    //TODO macwm screensaver
+    // Unnecessary.
   #elif PS_USE_mswm
-    //TODO mswm screensaver
+    // Unnecessary.
   #endif
   return 0;
 }

commit 01ea70edcaf710c1acd2e98b2f5315fd70f9a445

e609468df77 broke input device detection. Fixed it.

etc/tool/install.sh
@@ -9,7 +9,7 @@ fi
 
 DST_EXE=$DST/bin/plundersquad
 DST_DATA=$DST/share/plundersquad/ps-data
-DST_DESKTOP=$DST/applications/plundersquad.desktop
+DST_DESKTOP=/usr/share/applications/plundersquad.desktop
 DST_INPUT_CONFIG=$DST/share/plundersquad/input.cfg
 DST_HIGHSCORES=$DST/share/plundersquad/highscores
 DST_GENERAL_CONFIG=$DST/share/plundersquad/plundersquad.cfg

etc/tool/uninstall.sh
@@ -4,4 +4,4 @@ PFX=/usr/local
 
 rm -f "$PFX/bin/plundersquad"
 rm -rf "$PFX/share/plundersquad"
-rm -f "$PFX/applications/plundersquad.desktop"
+rm -f "/usr/share/applications/plundersquad.desktop"

src/gui/menus/ps_widget_heropanel.c
@@ -466,6 +466,10 @@ static int ps_heropanel_dismiss_menu(struct ps_widget *widget) {
 
 static int ps_heropanel_click_in(struct ps_widget *widget) {
   WIDGET->clicked_in=1;
+  ps_log(GUI,INFO,"ps_heropanel_click_in device=%p",WIDGET->device);
+  if (WIDGET->device) {
+    ps_log(GUI,INFO,"'%s' map=%p",WIDGET->device->name,WIDGET->device->map);
+  }
   if (WIDGET->device->map) {
     if (ps_heropanel_rebuild_CHOOSING(widget)<0) return -1;
   } else {

src/input/ps_input_maptm.c
@@ -66,8 +66,12 @@ static int ps_input_maptm_all_buttons_exist_on_device_cb(
   struct ps_input_maptm_all_buttons_exist_on_device_ctx *ctx=userdata;
   int fldp=ps_input_maptm_fld_search(ctx->maptm,btncfg->srcbtnid);
   if (fldp<0) return 0;
+  while (fldp&&(ctx->maptm->fldv[fldp-1].srcbtnid==btncfg->srcbtnid)) fldp--;
   if (fldp>=128) return 0;
-  ctx->bits[fldp>>3]|=(1<<(fldp&7));
+  while ((fldp<ctx->maptm->fldc)&&(fldp<128)&&(ctx->maptm->fldv[fldp].srcbtnid==btncfg->srcbtnid)) {
+    ctx->bits[fldp>>3]|=(1<<(fldp&7));
+    fldp++;
+  }
   return 0;
 }
  
@@ -84,7 +88,9 @@ static int ps_input_maptm_all_buttons_exist_on_device(const struct ps_input_mapt
   int i=maptm->fldc;
   if (i>=128) i=127;
   while (i-->0) {
-    if (!(ctx.bits[i>>3]&(1<<(i&7)))) return 0;
+    if (!(ctx.bits[i>>3]&(1<<(i&7)))) {
+      return 0;
+    }
   }
   
   return 1;

src/os/ps_log.c
@@ -12,7 +12,7 @@ int ps_log_level_by_domain[PS_LOG_DOMAIN_COUNT]={
   _(AUDIO         ,INFO)
   _(VIDEO         ,INFO)
   _(GENERATOR     ,INFO)
-  _(INPUT         ,INFO)
+  _(INPUT         ,DEBUG)
   _(MAIN          ,INFO)
   _(EDIT          ,INFO)
   _(MACWM         ,INFO)